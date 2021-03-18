0d4c5795666d1a159cbbdcd15ec1ba41f673bef3
amanmoar <amanmoar999@gmail.com>
2021-03-18 14:56:57 +0530

    added interfaces

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,5 +1,12 @@
 import * as express from "express"
 
+interface EncodedUrlParams {
+    url: string
+}
+
+interface DecodeUrlParams {
+    randomString: string
+}
 // Use DB if we want to persist the data and want the functionality works well even after app is restarted
 namespace UrlController {
   const dict = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -10,29 +17,30 @@ namespace UrlController {
     req: express.Request,
     res: express.Response
   ) => {
-    const { url } = req.body
+    const { url } = req.body as EncodedUrlParams
 
     if (!url) {
       res.send("URL is required param").status(400)
     }
 
     // logic starts from here
-    if (long2short.has(url)) {
+    const alreadyPresentShortUrl: string | undefined = long2short.get(url)
+    if (alreadyPresentShortUrl) {
       res
         .send({
-          url: `${process.env.APP_BASE_URL + long2short.get(url)}`
+          url: `${process.env.APP_BASE_URL + alreadyPresentShortUrl}`
         })
         .status(200)
       return
     }
 
-    let idx = 0
+    let idx: number = 0
     const randomStringArray: string[] = []
-    for (let i = 0; i < 6; i++) {
+    for (let i: number = 0; i < 6; i++) {
       randomStringArray[i] = dict[(Math.random() * 61).toFixed()]
     }
 
-    let randomString = randomStringArray.join("")
+    let randomString: string = randomStringArray.join("")
     while (short2long.has(randomString)) {
       randomStringArray[idx] = dict[(Math.random() * 61).toFixed()]
       idx = (idx + 1) % 6
@@ -48,8 +56,8 @@ namespace UrlController {
     req: express.Request,
     res: express.Response
   ) => {
-    const { randomString } = req.params
-    const url = short2long.get(randomString)
+    const { randomString } = req.params as DecodeUrlParams
+    const url: string | undefined = short2long.get(randomString)
     if (!url) {
       res.send("No such encoding").status(409)
       return
