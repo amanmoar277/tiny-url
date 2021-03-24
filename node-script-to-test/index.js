c41d9625eece889f87e95ee6bc16d8246345ce32
amanmoar <amanmoar999@gmail.com>
2021-03-24 10:07:09 +0530

    added node-script without async await

--- node-script-to-test/index.js
+++ node-script-to-test/index.js
@@ -1,9 +1,9 @@
 const fetch = require("node-fetch");
 
 async function encode(){
-    const promiseArray = [];
+    const resultArray = [];
     for(let i = 0; i < 10; i++) {
-        const pendingPromise = fetch("http://localhost:5000/api/encode", { 
+        const data = await fetch("http://localhost:5000/api/encode", { 
             // Adding method type 
             method: "POST", 
             
@@ -18,11 +18,10 @@ async function encode(){
             } 
         })
     
-        promiseArray.push(pendingPromise)
+        resultArray.push(data);
     }
-    const result = await Promise.all(promiseArray);
 
-    const jsonResponse = await Promise.all(result.map(x => x.json()));
+    const jsonResponse = await Promise.all(resultArray.map(x => x.json()));
     const urls = await Promise.all(jsonResponse.map(x => x.url));
     console.log(urls);
     
@@ -30,10 +29,10 @@ async function encode(){
 }
 
 async function decode(urls){
-    const promiseArray = [];
-    urls = urls.map(url => url.replace('https://tiny-url-service.com/', 'http://localhost:5000/'))
-    urls.forEach(url => {
-        const pendingPromise = fetch(url+'', { 
+    // const resultArray = [];
+    const modifiedUrls = urls.map(url => url.replace('https://tiny-url-service.com/', 'http://localhost:5000/'))
+    const resultArray = await Promise.all(modifiedUrls.map( url => 
+        fetch(url+'', { 
             // Adding method type 
             method: "GET", 
             
@@ -41,13 +40,11 @@ async function decode(urls){
             headers: { 
                 "Content-type": "application/json; charset=UTF-8"
             } 
-        }).catch(e => console.log('Hi'))
-    
-        promiseArray.push(pendingPromise)
-    })
-    const result = await Promise.all(promiseArray);
+        })
+    ))
 
-    result.forEach(async x => {
+    const result = await Promise.all(resultArray);
+    result.map(async x => {
         try {
             const json = await x.json()
             console.log('Not Redirected for -> ' + x.url)
@@ -58,8 +55,15 @@ async function decode(urls){
 }
 
 async function main() {
+    const startEncode = (new Date()).getTime();
     const urls = await encode();
+    const endEncode = (new Date()).getTime();
+    console.log(`Time taken by encoding -> ${endEncode - startEncode} ms`);
+
+    const startDecode = (new Date()).getTime();
     await decode(urls);
+    const endDecode = (new Date()).getTime();
+    console.log(`Time taken by decoding -> ${endDecode - startDecode} ms`);
 }
 
 main();
